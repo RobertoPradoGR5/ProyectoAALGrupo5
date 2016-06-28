6abef07b558c68e6b54d7f4211a6af2a7a5834ec
JulioMora <juliocmorab@hotmail.com>
2016-06-28 18:29:06 -0500

    Interfaz de Calculo de Calorias

---
+++ FeedingEPN/app/src/main/java/com/aal/epn/feedingepn/Calculo_Calorias.java
@@ -0,0 +1,13 @@
+package com.aal.epn.feedingepn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calculo_Calorias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculo__calorias);
+    }
+}
