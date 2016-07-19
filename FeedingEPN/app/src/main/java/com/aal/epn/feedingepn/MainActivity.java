493b34521649eb48fa7cea6c2e4509136329095e
davdevelo_dvd@hotmail.com <Maribel_1989>
2016-07-19 18:51:01 -0500

    Aplicación de material design

--- FeedingEPN/app/src/main/java/com/aal/epn/feedingepn/MainActivity.java
+++ FeedingEPN/app/src/main/java/com/aal/epn/feedingepn/MainActivity.java
@@ -4,12 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
     }
-}
